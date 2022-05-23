a68d13b4d8e339f4d0719b51dbe8e32b0528af78
Kirill-Kuznetsov-git <kirill.cuznetsov200@yandex.ru>
2022-05-23 21:30:29 +0300

    update deploy.ts file

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,29 +1,26 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
+
 import { ethers } from "hardhat";
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
 
-  // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const NFT721Factory = await ethers.getContractFactory("NFT721");
+  const NFT721 = await NFT721Factory.deploy("TestToken", "TT");
 
-  await greeter.deployed();
+  const NFT1155Factory = await ethers.getContractFactory("NFT1155");
+  const NFT1155 = await NFT1155Factory.deploy("uri");
 
-  console.log("Greeter deployed to:", greeter.address);
+  const MarketPlaceFactory = await ethers.getContractFactory("NFT721");
+  const MarketPlace = await MarketPlaceFactory.deploy("TestToken", "TT");
+
+  await NFT721.deployed();
+  console.log("NFT721 deployed to:", NFT721.address);
+  await NFT1155.deployed();
+  console.log("NFT1155 deployed to:", NFT1155.address);
+  await MarketPlace.deployed();
+  console.log("MarketPlace deployed to:", MarketPlace.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
+
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
